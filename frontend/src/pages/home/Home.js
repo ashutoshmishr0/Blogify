c37ba0a9f1c871c4ff028ec91a1558da5e5a9251
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2024-07-15 22:21:46 +0530

    ok

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts" + search);
+      const res = await axios.get("/posts");
       setPosts(res.data);
     };
     fetchPosts();
