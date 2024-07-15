6c53c61a70d58f9bc52c38baa7faed82e394242f
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2024-07-15 21:24:54 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,11 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-  // <BrowserRouter>
+  <BrowserRouter>
     <ContextProvider>
     <App />
     </ContextProvider>
-  // </BrowserRouter>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
