7e48e48d2d7d290fb11f65996ccef7b64b4fc536
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2024-07-15 08:16:23 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,16 +9,16 @@ import {BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  // <React.StrictMode>
-  <BrowserRouter>
+  <React.StrictMode>
+  // <BrowserRouter>
     <ContextProvider>
     <App />
     </ContextProvider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  // </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitalreportWebVitals();
-// reportWebVitals();
+reportWebVitals();
