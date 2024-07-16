a693512d32a6ddd3075a5ed524dbf6c44d021383
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2024-07-16 07:05:34 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -48,7 +48,7 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-app.use("/api/auth", authRoute);
+app.use("/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
